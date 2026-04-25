cbcfc3acabf6f27f8ec2efafd8472f852e7237fc
Ibrahima Diallo <ibrahimadialloib300@gmail.com>
2026-04-25 12:26:38 +0000

    feat(ui-home): page d'accueil + liste prestataires

---
+++ frontend/src/app/features/client/layout/header/header.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { RouterModule, Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../../../../core/services/auth.service';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [RouterModule, CommonModule],
+  templateUrl: './header.html',
+})
+export class HeaderComponent {
+  menuOpen = false;
+  user: any = JSON.parse(localStorage.getItem('user') || '{}');
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  logout() {
+    this.authService.logout();
+  }
+}
