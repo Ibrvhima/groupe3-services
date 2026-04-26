13adf146ff935d326402e527e99e09b1ece4e090
Ibrahima Diallo <ibrahimadialloib300@gmail.com>
2026-04-26 00:07:32 +0000

    feat(api-demandes): modèle Demande + API avec permissions

--- backend/apps/demandes/serializers.py
+++ backend/apps/demandes/serializers.py
@@ -1,4 +1,14 @@
 from rest_framework import serializers
-# from .models import ...
+from .models import Demande
 
-# TODO: Définir les serializers pour l'app demandes
+
+class DemandeSerializer(serializers.ModelSerializer):
+    client_nom        = serializers.CharField(source='client.nom', read_only=True)
+    client_prenom     = serializers.CharField(source='client.prenom', read_only=True)
+    prestataire_nom   = serializers.CharField(source='prestataire.user.nom', read_only=True)
+    prestataire_prenom= serializers.CharField(source='prestataire.user.prenom', read_only=True)
+
+    class Meta:
+        model  = Demande
+        fields = '__all__'
+        read_only_fields = ['client', 'statut', 'date_creation']
