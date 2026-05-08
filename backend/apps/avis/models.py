cc39f7e35a50ce8c0040e32276904269bec6f6a2
Ibrahima Diallo <ibrahimadialloib300@gmail.com>
2026-05-08 10:54:26 +0000

    feat(api-avis): modèle Avis + signal note moyenne + API complète

--- backend/apps/avis/models.py
+++ backend/apps/avis/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+from apps.users.models import User
+from apps.prestataires.models import Prestataire
+from apps.demandes.models import Demande
 
-# TODO: Définir les modèles pour l'app avis
+
+class Avis(models.Model):
+    demande     = models.OneToOneField(Demande, on_delete=models.CASCADE)
+    client      = models.ForeignKey(User, on_delete=models.CASCADE)
+    prestataire = models.ForeignKey(Prestataire, on_delete=models.CASCADE)
+    note        = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)]
+    )
+    commentaire = models.TextField()
+    date        = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Avis {self.note}/5 par {self.client.nom}'
